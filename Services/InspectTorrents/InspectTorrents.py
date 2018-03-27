e198a7bc6062008bea9e6b21325f831cc1492d11
Renato Samperio <renatosamperio@gmail.com>
2018-03-27 15:36:51 +0200

    INSPECT_TORRETS: Re-formatted indentation

--- Services/InspectTorrents/InspectTorrents.py
+++ Services/InspectTorrents/InspectTorrents.py
@@ -19,127 +19,132 @@ logging.getLogger("chardet.charsetprober").setLevel(logging.WARNING)
 logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
 
 class InspectTorrents(threading.Thread):
-  def __init__(self, **kwargs):
-    '''Service task constructor'''
-    # Initialising thread parent class
-    threading.Thread.__init__(self)
-    
-    try:
-      # Initialising class variables
-      self.component	= self.__class__.__name__
-      self.logger	= Utilities.GetLogger(self.component)
-      
-      # Thread action variables
-      self.tStop 	= threading.Event()
-      self.threads	= []
-      self.tid		= None
-      self.running	= False
-      
-      ## Adding local variables
-      self.service	= None
-      self.onStart	= True
-      self.not_searching= False
+    def __init__(self, **kwargs):
+        '''Service task constructor'''
+        # Initialising thread parent class
+        threading.Thread.__init__(self)
+        
+        try:
+            # Initialising class variables
+            self.component	= self.__class__.__name__
+            self.logger	= Utilities.GetLogger(self.component)
+            
+            # Thread action variables
+            self.tStop 	= threading.Event()
+            self.threads	= []
+            self.tid		= None
+            self.running	= False
+            
+            ## Adding local variables
+            self.service	= None
+            self.onStart	= True
+            self.not_searching= False
+            
+            ## Local variables
+            self.search_item  = None
+            
+            # Generating instance of strategy
+            for key, value in kwargs.iteritems():
+                if "service" == key:
+                  self.service = value
+                elif "onStart" == key:
+                  self.onStart = bool(value)
+                elif "search_item" == key:
+                  self.search_item = value
+                  print self.search_item
+            ## Setting item started for reporting to device action
+            self.running	= True
+            
+            # Starting action thread
+            if self.onStart:
+                self.logger.debug("  + Process is starting immediately")
+                self.start()
+            
+            ## Adding monitor thread to the list of references
+            self.threads.append(self)
+        except Exception as inst:
+            Utilities.ParseException(inst, logger=self.logger)
 
-      # Generating instance of strategy
-      for key, value in kwargs.iteritems():
-	if "service" == key:
-	  self.service = value
-	elif "onStart" == key:
-	  self.onStart = bool(value)
-	  
-      ## Setting item started for reporting to device action
-      self.running	= True
-	
-      # Starting action thread
-      if self.onStart:
-	self.logger.debug("  + Process is starting immediately")
-	self.start()
-      
-      ## Adding monitor thread to the list of references
-      self.threads.append(self)
-    except Exception as inst:
-      Utilities.ParseException(inst, logger=self.logger)
+    def hasStarted(self):
+        ''' Reports task thread status'''
+        return self.running and not self.tStop.isSet()
 
-  def hasStarted(self):
-    ''' Reports task thread status'''
-    return self.running and not self.tStop.isSet()
-
-  def hasFinished(self):	
-    ''' Reports task thread status'''
-    return not self.running and self.tStop.isSet()
+    def hasFinished(self):	
+        ''' Reports task thread status'''
+        return not self.running and self.tStop.isSet()
   
-  def run(self):
-    '''Threaded action '''
-    try:
-      ## Setting process as started
-      self.running	= True
-      
-      ## Setting thread for joining service list
-      if self.service is not None and self.service.task is not None:
-	self.logger.debug('  + Adding thread for joining context')
-	self.service.task.AddContext(self, self.component)
-      
-      # Getting thread GetPID
-      self.tid		= Utilities.GetPID()
-      self.logger.debug('Starting task [%s] with PID [%d]'% (self.component, self.tid))
-      
-      self.logger.debug('Looping for task [%d]'%self.tid)
-      while not self.tStop.isSet():
-        if self.not_searching:
-          self.logger.debug('   ~~~ Starting action handler action')
-	# Do something here!
-	# ...
-          self.logger.debug('   ~~~ Posting results')
-          self.service.notify("updated", result_state, items=itemsCall)
+    def run(self):
+        '''Threaded action '''
+        try:
+            ## Setting process as started
+            self.running    = True
+          
+            ## Setting thread for joining service list
+            if self.service is not None and self.service.task is not None:
+                self.logger.debug('  + Adding thread for joining context')
+                self.service.task.AddContext(self, self.component)
+          
+            # Getting thread GetPID
+            self.tid        = Utilities.GetPID()
+            self.logger.debug('Starting task [%s] with PID [%d]'% (self.component, self.tid))
+          
+            self.logger.debug('Looping for task [%d]'%self.tid)
+            while not self.tStop.isSet():
+                if self.not_searching:
+                    self.logger.debug('   ~~~ Starting action handler action')
+                    # Do something here!
+                    # ...
+                    self.logger.debug('   ~~~ Posting results')
+                    self.service.notify("updated", result_state, items=itemsCall)
+                    
+                    self.logger.debug('   ~~~ Resetting search query...')
+                    self.not_searching = False
+            self.tStop.wait(5)
+        
+            # Ending thread routine
+            self.running = False
+            self.logger.debug('Ending thread [%d]'%self.tid)
+          
+        except Exception as inst:
+            Utilities.ParseException(inst, logger=self.logger)
 
-          self.logger.debug('   ~~~ Resetting search query...')
-          self.not_searching = False
-	self.tStop.wait(5)
-	
-      # Ending thread routine
-      self.running = False
-      self.logger.debug('Ending thread [%d]'%self.tid)
-      
-    except Exception as inst:
-      Utilities.ParseException(inst, logger=self.logger)
-    
-  def close(self):
-    ''' Ending task service'''
-    try:
-      # Do additional stopping routines here
-      # ...
-      
-      if not self.tStop.isSet():
-	# Stop thread and give some time to do whatever it has to do
-	self.logger.debug(" Stopping task thread, setting event...")
-	self.tStop.set()
-	time.sleep(0.75)
-      else:
-	self.logger.debug(" Event loop is already interrupted")
-      
-      # Force to stop before leaving
-      if self.is_alive() and self.service is not None:  
-	self.logger.debug( "  Stopping the thread and wait for it to end")
-	threading.Thread.join(self, 1)  
-      else:
-	self.logger.debug(" Thread is not alive")
-	
-      self.logger.debug( "  Thread stopped")
-      
-    except Exception as inst:
-      Utilities.ParseException(inst, logger=self.logger)
+    def close(self):
+        ''' Ending task service'''
+        try:
+            # Do additional stopping routines here
+            # ...
+          
+            if not self.tStop.isSet():
+                # Stop thread and give some time to do whatever it has to do
+                self.logger.debug(" Stopping task thread, setting event...")
+                self.tStop.set()
+                time.sleep(0.75)
+            else:
+                self.logger.debug(" Event loop is already interrupted")
+          
+          # Force to stop before leaving
+            if self.is_alive() and self.service is not None:  
+                self.logger.debug( "  Stopping the thread and wait for it to end")
+                threading.Thread.join(self, 1)  
+            else:
+                self.logger.debug(" Thread is not alive")
+        
+            self.logger.debug( "  Thread stopped")
+          
+        except Exception as inst:
+            Utilities.ParseException(inst, logger=self.logger)
 
-  def execute(self, service):
-    ''' Execute InspectTorrents task by calling a "run" method in the service'''
+    def execute(self, service):
+        ''' Execute InspectTorrents task by calling a "run" method in the service'''
 
-  def SetSearch(self, status):
-    try:
-      ## Getting correct action
-      VARIABLE1      = status['VARIABLE1']
-      VARIABLE2      = status['VARIABLE2']
-      self.not_searching = True
-    except Exception as inst:
-      Utilities.ParseException(inst, logger=self.logger)
+    def SetSearch(self, status):
+        try:
+            ## Getting correct action
+            VARIABLE1      = status['VARIABLE1']
+            VARIABLE2      = status['VARIABLE2']
+            self.not_searching = True
+        except Exception as inst:
+            Utilities.ParseException(inst, logger=self.logger)
 
 ## Standalone main method
 LOG_NAME = 'InspectTorrents'
